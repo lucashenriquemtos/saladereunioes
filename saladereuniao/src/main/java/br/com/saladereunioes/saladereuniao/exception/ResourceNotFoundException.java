3ed0be98be5d4ef53fb95519423a16e6170f944d
Lucas Henrique de Oliveira Matos <lucasmatos@pactosolucoes.com.br>
2021-08-25 20:12:14 -0300

    Criando ResourceNotFoundException.java

--- saladereuniao/src/main/java/br/com/saladereunioes/saladereuniao/exception/ResourceNotFoundException.java
+++ saladereuniao/src/main/java/br/com/saladereunioes/saladereuniao/exception/ResourceNotFoundException.java
@@ -1,4 +1,14 @@
 package br.com.saladereunioes.saladereuniao.exception;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
+
+    private static final Long serialVersionUID = 1L;
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
 }
