5f80e179a13f34cf5d189e621be21c38aa3d88a9
Lucas Henrique de Oliveira Matos <lucasmatos@pactosolucoes.com.br>
2021-08-25 21:55:45 -0300

    Criando component

--- client-room/src/app/room-list/room-list.component.ts
+++ client-room/src/app/room-list/room-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RoomDetaisComponent} from "../room-detais/room-detais.component";
+import {Observable} from "rxjs";
+import {RoomService} from "../room.service";
+import {Room} from "../room";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {error} from "@angular/compiler/src/util";
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +13,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
+  rooms: Observable<Room[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private room: RoomService, private router: Router) {
   }
 
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  private reloadData() {
+    this.room = this.roomService.getRoomList();
+  }
+
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+      .subscribe(
+        data => {
+          console.log(data)
+          this.reloadData();
+        },
+        error => console.log(error)
+      );
+  }
+
+  roomDetails(id: number) {
+    this.router.navigate(['datails'], id);
+  }
+
+  updateRoom(id: number) {
+    this.router.navigate(['uptade'], id);
+  }
 }
